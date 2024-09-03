ebde6b5e24d788326db2a71482da096e6a189e97
Emperordagoat2 <aliciacecelia66@gmail.com>
2024-09-03 17:32:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -159,7 +159,25 @@ async function startnigg(phone) {
           await negga.sendMessage(
             negga.user.id,
             {
-              text: 'Hello there! 👋 \n\nDo not share your session id with anyone.\n\nPut the above in SESSION_ID var\n\nThanks for using GURU-BOT\n\n join support group:- https://chat.whatsapp.com/JY4R2D22pbLIKBMQWyBaLg \n',
+              text: '*𝐉𝐎𝐇𝐍𝐋𝐄𝐎-𝐌𝐃 𝐈𝐒 𝐒𝐄𝐒𝐒𝐈𝐎𝐍 𝐂𝐎𝐍𝐍𝐄𝐂𝐓𝐄𝐃 𝐒𝐔𝐂𝐂𝐄𝐒𝐒𝐅𝐔𝐋𝐋𝐘*
+> Copy the above session Id and deploy *JOHNLEO-MD*.
+
+*𝐅𝐎𝐋𝐋𝐎𝐖 𝐎𝐔𝐑 𝐎𝐅𝐅𝐈𝐂𝐈𝐀𝐋 𝐖𝐀.𝐂𝐇𝐀𝐍𝐍𝐄𝐋* 
+> https://whatsapp.com/channel/0029VamlYh41SWssaEvw4E2i
+
+*𝐅𝐎𝐋𝐋𝐎𝐖 𝐌𝐄 𝐎𝐍 𝐆𝐈𝐓𝐇𝐔𝐁*
+> https://github.com/johnleosmith
+
+*𝐉𝐎𝐈𝐍 𝐓𝐇𝐄 𝐖𝐀 𝐃𝐈𝐒𝐂𝐔𝐒𝐒𝐈𝐎𝐍 𝐆𝐑𝐎𝐔𝐏*
+> https://chat.whatsapp.com/GkOeZ2eLZhx6lenAq1z83o
+
+*𝐅𝐎𝐋𝐋𝐎𝐖 𝐎𝐔𝐑 𝐓𝐄𝐋𝐄𝐆𝐑𝐀𝐌 𝐂𝐇𝐀𝐍𝐍𝐄𝐋*
+> https://t.me/JohnleoTech
+
+*𝐅𝐎𝐋𝐋𝐎𝐖 𝐎𝐔𝐑 𝐘𝐎𝐔𝐓𝐔𝐁𝐄 𝐂𝐇𝐀𝐍𝐍𝐄𝐋*
+> https://www.youtube.com/@JohnleoTech
+
+> ✅✅ 𝐏𝐎𝐖𝐄𝐑𝐄𝐃 𝐁𝐘 𝐉𝐎𝐇𝐍𝐋𝐄𝐎 𝐓𝐄𝐂𝐇✅✅',
             },
             { quoted: guru }
           )
